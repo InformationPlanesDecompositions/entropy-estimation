ddb14676af1dca0c203290e8506ac89037b5d6cb
mxi <15906207+halvlys@users.noreply.github.com>
2025-07-10 10:04:57 +0200

    Add temp confidence interval computation for plug-in

--- estimator/plug_in.py
+++ estimator/plug_in.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy as sp
 
 
 def estimate_entropy(samples: np.ndarray) -> float:
@@ -25,3 +26,31 @@ def estimate_entropy_variance(
         variance += p * np.log2(p) ** 2
 
     return variance / len(samples)
+
+
+def compute_entropy_confidence_interval(
+    samples: np.ndarray,
+    confidence: float = 0.95,
+    as_dict: bool = False
+) -> tuple[float, tuple[float, float]] | dict[str, float | tuple]:
+    h_hat = estimate_entropy(samples)
+    var_hat = estimate_entropy_variance(samples, h_hat)
+    std_hat = np.sqrt(var_hat)
+
+    # TODO: Kinda temp, check which distribution to use
+    t_score = sp.stats.t.pdf((1 + confidence) / 2, len(samples) - 1)
+
+    interval = (
+        h_hat - t_score * std_hat,
+        h_hat + t_score * std_hat
+    )
+
+    if as_dict:
+        res = {
+            'entropy': h_hat,
+            'confidence': interval
+        }
+    else:
+        res = (h_hat, interval)
+
+    return res
