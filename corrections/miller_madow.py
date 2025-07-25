b5ae95fedb900de5d9e7623c05aff58d926bb35d
mxi <15906207+halvlys@users.noreply.github.com>
2025-07-25 10:50:55 +0200

    Adjust 1st order MM for 1d or 2d array

--- corrections/miller_madow.py
+++ corrections/miller_madow.py
@@ -8,13 +8,24 @@ Applied as H^MM = H^hat + MM
 import numpy as np
 
 
+# TODO:
+#   - Add axis keyword to work on (M, N, D) arrays
 def first_order(samples: np.ndarray, n_classes: int | None = None) -> float:
+    if len(samples.shape) > 2:
+        raise ValueError('Currently only supported for 2-dim arrays')
+    
+    if len(samples.shape) == 0:
+        return 0
+
     if n_classes is None:
-        n_classes = len(np.unique(samples))
+        n_classes = len(np.unique(samples, axis=0))
 
-    return (n_classes - 1) / (2 * len(samples))
+    return (n_classes - 1) / (2 * samples.shape[0])
 
 
+# TODO:
+#   - Check if this even works for higher dimensions
+#   - Adjust for new first order method
 def second_order(
     samples: np.ndarray,
     probabilities: np.ndarray,
