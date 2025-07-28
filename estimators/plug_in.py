1ed27a45d925155845505cd4f492694c25649b72
mxi <15906207+halvlys@users.noreply.github.com>
2025-07-28 09:36:14 +0200

    Add shape checks for entropy estimation

--- estimators/plug_in.py
+++ estimators/plug_in.py
@@ -1,7 +1,14 @@
 import numpy as np
 
 
+# TODO: Adjust to multi-dim array to return entropy per experiment (i.e. 0 index)
 def estimate_entropy(samples: np.ndarray) -> float:
+    if len(samples.shape) > 2:
+        raise ValueError(f'Currently only supported for 1 or 2-dim arrays')
+    
+    if len(samples.shape) == 0:
+        return 0
+
     _, c = np.unique(samples, axis=0, return_counts=True)
 
     p = c / samples.shape[0]
