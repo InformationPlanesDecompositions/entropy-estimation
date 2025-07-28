13084673a02e27db5649a0b5f046334b49f2f7e6
mxi <15906207+halvlys@users.noreply.github.com>
2025-07-28 10:33:55 +0200

    Update variance estimate for plug-in

--- estimators/plug_in.py
+++ estimators/plug_in.py
@@ -9,27 +9,36 @@ def estimate_entropy(samples: np.ndarray) -> float:
     if len(samples.shape) == 0:
         return 0
 
-    _, c = np.unique(samples, axis=0, return_counts=True)
-
-    p = c / samples.shape[0]
-
+    p = _compute_observed_rates(samples)
     p = p[p != 0]
 
     return -np.sum(p * np.log2(p))
 
 
-# TODO: Adjust to multidimensional RV/joint entropy?
 def estimate_entropy_variance(
     samples: np.ndarray,
     entropy_hat: float | None = None
 ) -> float:
+    if len(samples.shape) > 2:
+        raise ValueError(f'Currently only supported for 1 or 2-dim arrays')
+    
+    if len(samples.shape) == 0:
+        return 0
+
+    # A bit inefficient but not important for now
     if entropy_hat is None:
         entropy_hat = estimate_entropy(samples)
 
-    variance = -entropy_hat ** 2
+    p = _compute_observed_rates(samples)
+    p = p[p != 0]
 
-    for _, count in zip(*np.unique_counts(samples)):
-        p = count / len(samples)
-        variance += p * np.log2(p) ** 2
+    variance = np.sum(p * np.log2(p) ** 2)
+    variance -= entropy_hat ** 2
 
-    return variance / len(samples)
+    return variance / samples.shape[0]
+
+
+def _compute_observed_rates(samples: np.ndarray) -> np.ndarray:
+    _, c = np.unique(samples, axis=0, return_counts=True)
+
+    return c / samples.shape[0]
