edf05bbc6cf4f92e4e62b7e52b285a72ca303945
mxi <15906207+halvlys@users.noreply.github.com>
2025-10-24 14:24:54 +0200

    Add method to evaluate ee on model data

--- main.py
+++ main.py
@@ -197,6 +197,89 @@ def _perform_mi_estimation(parser: argparse.ArgumentParser, args: argparse.Names
     ax.figure.colorbar(cmap, ax=sct_ax)
 
     fig.tight_layout()
+
+    if args.save:
+        output_dir = f'./output/mi/{dir_name}'
+
+        os.makedirs(output_dir, exist_ok=True)
+        plt.savefig(path.join(output_dir, 'information_plane.png'))
+
+    plt.show(block=True)
+    
+
+def _compare_entropy(parser: argparse.ArgumentParser, args: argparse.Namespace):
+    data_dir = args.data
+
+    if not path.isdir(data_dir):
+        parser.error(f'Please provide an existing directory, did not find {data_dir}')
+
+    activation_path = path.join(data_dir, 'activations.h5')
+
+    if not (path.exists(activation_path) and path.isfile(activation_path)):
+        parser.error(f'No <activations.h5> found in given directory')
+
+    # dir_name = path.basename(path.dirname(activation_path))
+
+    # activation_file = h5py.File('../ma-bnn-training/output/szt-8000/activations.h5', 'r')
+    activation_file = h5py.File(activation_path, 'r')
+
+    data = defaultdict(list)
+    rng = np.random.default_rng(2620)
+
+    # type: ignore
+    for epoch_data in tqdm(activation_file.values(), ncols=100, ascii=True):
+        epoch_data: h5py.Group
+        epoch_idx = epoch_data.attrs['epoch_idx']
+
+        for _, layer_data in epoch_data.items():
+            layer_idx = layer_data.attrs['layer_idx']
+            if not layer_data.attrs['is_packed']:
+                continue
+            data['Epoch'].append(epoch_idx)
+            data['Layer'].append(layer_idx)
+
+            t = np.unpackbits(layer_data[:])
+            t = t.reshape(-1, *layer_data.attrs['shape'])
+
+            n, dim = t.shape
+
+            t_int = t.dot(1 << np.arange(dim - 1, -1, -1))
+            p_t = np.bincount(t_int) / n
+            h_t = -np.sum(p_t * np.log2(p_t + 1e-12), where=~np.isclose(p_t, 0))
+
+            t_shuffle = np.apply_along_axis(rng.permutation, 0, t)
+            t_shuffle_int = t_shuffle.dot(1 << np.arange(dim - 1, -1, -1))
+            p_shuffle = np.bincount(t_shuffle_int) / n
+            h_t_shuffle = -np.sum(p_shuffle * np.log2(p_shuffle + 1e-12), where=~np.isclose(p_shuffle, 0))
+
+            p_neurons = np.apply_along_axis(np.bincount, 1, t.T) / n
+            h_neurons = -np.sum(p_neurons * np.log2(p_neurons + 1e-12), where=~np.isclose(p_neurons, 0))
+
+            data['HT'].append(h_t)
+            data['HShuffle'].append(h_t_shuffle)
+            data['HNeurons'].append(h_neurons.sum())
+
+    df_data = pd.DataFrame.from_dict(data, orient='columns')
+
+    fig, axes = plt.subplots(2, 2, sharex=True, sharey=True)
+    axes = axes.ravel()
+
+    for layer_idx in range(4):
+        ax = axes[layer_idx]
+
+        df_layer = df_data[df_data['Layer'] == layer_idx]
+        sns.lineplot(data=df_layer, x='Epoch', y='HT', label=r'normal', ls='-', ax=ax, legend=False)
+        sns.lineplot(data=df_layer, x='Epoch', y='HShuffle', label=r'shuffled', ls=':', ax=ax, legend=False)
+        sns.lineplot(data=df_layer, x='Epoch', y='HNeurons', label=r'$\sum_\ell H(T_\ell)$', ls='--', ax=ax, legend=False)
+
+        ax.set_title(f'Layer {layer_idx}')
+        ax.set_xlabel('Epoch')
+        ax.set_ylabel(r'$H(T)$')
+
+    handles, labels = axes[0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='center right')
+
+    plt.subplots_adjust(right=0.8)
     plt.show(block=True)
 
 
@@ -208,6 +291,8 @@ def main():
         case 'evaluate':
             if args.eval_target == 'toy':
                 _perform_evaluation(parser, args)
+            else:
+                _compare_entropy(parser, args)
         case 'mi' | 'mutual-information':
             _perform_mi_estimation(parser, args)
 
